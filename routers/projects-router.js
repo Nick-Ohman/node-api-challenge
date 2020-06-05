53657d0d375c59b06d591845c0455e7c14717b29
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-05 11:24:56 -0500

    projects middleware done

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -3,7 +3,55 @@ const Projects = require('../data/helpers/projectModel');
 const Actions = require('../data/helpers/actionModel');
 
 
+router.get('/', (req, res) => {
+    Projects.get()
+    .then(projects => {
+        res.status(200).json({
+            status: "success",
+            projects: projects
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Server Error",
+            error: error
+        })
+    })
+})
 
+// middleware
+
+function checkID(req, res, next){
+    const id = req.params.id;
+    Projects.get(id)
+    .then(project => {
+        if(project === null){
+            res.status(404).json({
+                status: "failure",
+                errorMessage: "No Project with that ID"
+            })
+        }else{
+            next();
+        }
+    })   
+   .catch(error => {
+       res.status(404).json({
+           error: error
+       })
+   })
+}
+
+function checkProjectBody(req, res, next){
+    const proName = req.body.name
+    const proDesc = req.body.description
+    if(!proName && typeof String || !proDesc && typeof String ){
+        res.status(400).json({
+            error: "Name and Description are required fields"
+        })
+    } else{
+        next();
+    }
+}
 
 
 module.exports = router;
